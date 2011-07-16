baf92694c465ff94269f74f5a721621dcd75886f
Kobito <kobito@orange.mykobito.net>
2011-07-16 19:40:55 +0900

    first commit. Have a fun with Rails!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hinagiku::Application.config.secret_token = '1b3f9034d8f2a93556cd9ab3606b4e56ce5d8d1ac6458098b67a2394c0f494287250b8be04350fceafe39e054a6ddc80fe768a33587e2992c29f1a9e4243258c'
---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+names = [
+         "切手を買う",
+         "報告書を書く",
+         "家賃を払う",
+         "猫のえさを買う",
+         "燃えないごみを出す"
+]
+
+description = "これは説明です。" * 20
+
+5.times do |n|
+  Task.create(:name => names[n], :description => description,
+              :due_date => (n - 2).days.from_now, :done => n.zero?)
+end
